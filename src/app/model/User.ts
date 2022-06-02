316fea61361346414aae7241d3c2c3b084fc7a49
Bárbara Salla <barbara.msalla@gmail.com>
2022-06-01 21:50:52 -0300

    adicionando camada service e model

---
+++ src/app/model/User.ts
@@ -0,0 +1,18 @@
+import { Postagem } from "./Postagems";
+
+export class User{
+    public id: number;
+
+    public nome: string;
+
+    public usuario: string;
+
+    public senha: string;
+
+    public foto: string;
+
+    public tipo: string;
+
+    public postagem: Postagem[]
+
+}
