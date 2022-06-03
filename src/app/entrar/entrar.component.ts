ca778569b87488b802572501c559eded3a9d6091
Bárbara Salla <barbara.msalla@gmail.com>
2022-06-03 13:56:12 -0300

    configurando componente inicio e adicionando componente tema

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -23,19 +23,22 @@ export class EntrarComponent implements OnInit {
   }
 
   entrar(){
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
-      this.userLogin = resp
+    this.auth.entrar(this.userLogin).subscribe({
 
+      next: (resp: UserLogin) => {
+      this.userLogin = resp
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
-
+      environment.tipo = this.userLogin.tipo
       this.router.navigate(['/inicio'])
-    }, erro =>{
-      if(erro.status == 500){
+      },
+      error: erro =>{
+      if(erro.status == 401){
         alert('Usuário ou senha estão incorretos.')
       }
-    })
+      }, 
+    });
   }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { CadastrarComponent } from '../cadastrar/cadastrar.component';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
@@ -13,11 +14,20 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://blogpessoal-bsm001.herokuapp.com/usuarios/logar', userLogin)
   }
 
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://blogpessoal-bsm001.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok=false
+
+    if(environment.token != ''){
+      ok=true
+    }
+    return ok
   }
 }
